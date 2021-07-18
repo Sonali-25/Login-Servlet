aa231364ddfaf295d465fe1a7242bb2649370bc7
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-18 19:36:20 +0530

    My First Servlet

--- src/main/java/com/magic/firstservlet/FirstServlet.java
+++ src/main/java/com/magic/firstservlet/FirstServlet.java
@@ -11,8 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/FirstServlet")
 public class FirstServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
+    
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
