9912f72b6fa0716fd7281a7838e0f61847f7a9bd
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-18 19:39:12 +0530

    My First Servlet

--- src/main/java/com/magic/firstservlet/FirstServlet.java
+++ src/main/java/com/magic/firstservlet/FirstServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/FirstServlet")
 public class FirstServlet extends HttpServlet {
-    
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
